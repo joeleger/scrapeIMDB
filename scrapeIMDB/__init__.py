9d3c657d4d249a2edca82929af3542a0e65ff5ce
joeleger <joeleger@users.noreply.github.com>
2020-06-13 17:14:50 -0400

    Added authentication and account page to app and implemented Flask-login package

--- scrapeIMDB/__init__.py
+++ scrapeIMDB/__init__.py
@@ -1,6 +1,8 @@
 from os import environ
 from dotenv import load_dotenv
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv('.env')
@@ -8,5 +10,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movieListing.db'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 from scrapeIMDB import routes
